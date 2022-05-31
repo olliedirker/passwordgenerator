9209ad330fe075bffc9040cbbc224554e15f9375
olliedirker <olliedirker@gmail.com>
2022-05-31 18:05:57 -0500

    time to ask proffessor

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 // Assignment code here
-function generatePassword(array){
-var password = array.length, randomiseCharacters;
-var randomiseCharacters ={
-if (upperCase) {password += randomiseCharacters.upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
-if (lowerCase) {password += randomiseCharacters.lowerCase = "abcdefghijklmnopqrstuvwxyz"},
-if (numberCase) {password += randomiseCharacters.numberCase = "1234567890"},
-if (symbolCase) {password += randomiseCharacters.symbolCase = "!@#$%^&*()_+{}|:<>?-=[]\;',./'"}
-};
+function writePassword(array) {
+  var password = array.length, randomiseCharacters;
+  var randomiseCharacters = {
+    if(upperCase) { password += randomiseCharacters.upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+    if(lowerCase) { password += randomiseCharacters.lowerCase = "abcdefghijklmnopqrstuvwxyz" },
+    if(numberCase) { password += randomiseCharacters.numberCase = "1234567890" },
+    if(symbolCase) { password += randomiseCharacters.symbolCase = "!@#$%^&*()_+{}|:<>?-=[]\;',./'" }
+  };
 
 }
-var passLength = Number(prompt('How long is your password required to be?'));
-while (isNaN(passLength) || passLength < 8
+var passLength = Number(prompt('How many characters is your password required to be?'));
+while (isNaN(passLength) || passLength < 8 || passlength > 128) passLength = Number(alert("Must be at least 8 chacters and a maximum of 128."));
+var upperCase = confirm('Would you like your password to contain uppercase letters?');
+var lowerCase = confirm('Would you like your password to contain lowercase letters?');
+var numberCase = confirm('Would you like your password to contain numbers?');
+var symbolCase = confirm('Would you like your password to contain special characters?');
 
-
+while (upperCase && lowerCase && numberCase && symbolCase) {
+  alert("You must at least pick one of the following");
+  upperCase = confirm("Would you like your password to contain uppercase letters");
+  lowerCase = confirm('Would you like your password to contain lowercase letters?');
+  numberCase = confirm('Would you like your password to contain numbers?');
+  symbolCase = confirm('Would you like your password to contain special characters?');
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
