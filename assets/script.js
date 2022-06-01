9c6c5142228b2ba447614d5379b21be25ab57eb7
olliedirker <olliedirker@gmail.com>
2022-05-31 19:06:49 -0500

    begin revision

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,33 @@
 // Assignment code here
-function writePassword(array) {
-  var password = array.length, randomiseCharacters;
-  var randomiseCharacters = {
-    if(upperCase) { password += randomiseCharacters.upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
-    if(lowerCase) { password += randomiseCharacters.lowerCase = "abcdefghijklmnopqrstuvwxyz" },
-    if(numberCase) { password += randomiseCharacters.numberCase = "1234567890" },
-    if(symbolCase) { password += randomiseCharacters.symbolCase = "!@#$%^&*()_+{}|:<>?-=[]\;',./'" }
+var generateBtn = document.querySelector("#generate");
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var numberCase = ["1234567890"];
+var symbolCase = ["!@#$%^&*()_+{}|:<>?-=[]\;',./'"];
+
+var finalPassword = [];
+
+function writePassword() {
+  var passLength = window.prompt('How many characters is your password required to be? It must be between 8 and 128 characters');
+
+  if (passLength) (passLength >= 8 || passLength >= 128){
+    var wantUpperCase = window.confirm('Would you like your password to contain uppercase letters?');
+    var wantLowerCase = window.confirm('Would you like your password to contain lowercase letters?');
+    var wantNumberCase = window.confirm('Would you like your password to contain numbers?');
+    var wantSymbolCase = window.confirm('Would you like your password to contain special characters?');
   };
 
-}
-var passLength = Number(prompt('How many characters is your password required to be?'));
-while (isNaN(passLength) || passLength < 8 || passlength > 128) passLength = Number(alert("Must be at least 8 chacters and a maximum of 128."));
-var upperCase = confirm('Would you like your password to contain uppercase letters?');
-var lowerCase = confirm('Would you like your password to contain lowercase letters?');
-var numberCase = confirm('Would you like your password to contain numbers?');
-var symbolCase = confirm('Would you like your password to contain special characters?');
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
 
-while (upperCase && lowerCase && numberCase && symbolCase) {
-  alert("You must at least pick one of the following");
-  upperCase = confirm("Would you like your password to contain uppercase letters");
-  lowerCase = confirm('Would you like your password to contain lowercase letters?');
-  numberCase = confirm('Would you like your password to contain numbers?');
-  symbolCase = confirm('Would you like your password to contain special characters?');
-}
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
-  passwordText.value = password;
+  }
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
