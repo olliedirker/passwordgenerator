c80a30facbfa43444a364fef13efb6d8c566f01f
olliedirker <olliedirker@gmail.com>
2022-08-31 22:09:37 -0500

    finished javascript'

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,70 @@
-// Assignment code here
+var letters = "abcdefghijklmnopqrstuvwyxz";
+var numbers = "1234567890";
+var specialChars = "!@#$%^&*()_+?><,./:;'{}[]|";
+// toUpperCase is a built in JavaScript function that will take our letters variable and make it all uppercase
+var uppercase = letters.toUpperCase();
+var password = [];
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var numberCase = ["1234567890"];
-var symbolCase = ["!@#$%^&*()_+{}|:<>?-=[]\;',./'"];
 
-var finalPassword = [];
-
+// Write password to the #password input
 function writePassword() {
-  var passLength = window.prompt('How many characters is your password required to be? It must be between 8 and 128 characters');
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  if (passLength) (passLength >= 8 || passLength >= 128){
-    var wantUpperCase = window.confirm('Would you like your password to contain uppercase letters?');
-    var wantLowerCase = window.confirm('Would you like your password to contain lowercase letters?');
-    var wantNumberCase = window.confirm('Would you like your password to contain numbers?');
-    var wantSymbolCase = window.confirm('Would you like your password to contain special characters?');
-  };
+  // password is an array, and we want to join everything to make a string.
+  var finalPassword = password.join("");
 
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
+  passwordText.value = finalPassword;
+}
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+function generatePassword() {
+  var passLength = window.prompt("How long do you want your password to be? Please pick between 8 and 128");
 
-    passwordText.value = password;
+  if (passLength >= 8 && passLength <= 128) {
+    // We are passing the value of the variable passLength into our passwordChoices function.
+    passwordChoices(passLength);
+  } else {
+    window.alert("Please pick a number between 8 and 128!");
+  }
+}
 
+function passwordChoices(passLength) {
+
+  //these are all of our prompts, which all pass true or false values. Okay returns true and cancel returns false.
+  var wantLower = window.confirm('Click okay for lowercase, cancel for no lowercase.');
+  var wantUpper = window.confirm('Click okay for uppercase, cancel for no uppercase.');
+  var wantNumber = window.confirm('Click okay for numbers, cancel for no numbers.');
+  var wantSpecial = window.confirm('Click okay for special characters, cancel for no special characters.');
+
+  // these conditional statements are basically finding what the users wants in their password, and adds it to our variable "x".
+  if (wantLower === true) {
+    var x = (letters);
+  } else if (wantLower === false) {
+    // we want to define x as an empty string if the first choice is false because we will not be able to concat our choices if we don't have a string to concat
+    var x = "";
+  } if (wantUpper === true) {
+    var x = x.concat(uppercase);
+  } if (wantNumber === true) {
+    var x = x.concat(numbers);
+  } if (wantSpecial === true) {
+    var x = x.concat(specialChars);
   }
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  // here we are passing x, which contains all of the characters that the user has selected. We are also passing passLength because we will need it for our next function.
+  randomPassword(x, passLength);
+
+}
+
+function randomPassword(x,passLength) {
+  // passLength is whatever number that the user has chosen for their password.
+  for (var i=0; i< passLength; i++) {
+    // our function will then keep looping and choosing a random character from our x string, which contains all of the user's choices.
+    var makePassword = x[Math.floor(Math.random() * x.length)];
+    // we have to push this into password, which is an empty array in order to save this value of makePassword.
+    password.push(makePassword);
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
